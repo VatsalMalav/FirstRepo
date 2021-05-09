beed84756b7eb1afda24170f5bff9ecb48969a5d
harshulvijay <devhvijay@gmail.com>
2021-05-10 01:45:30 +0530

    build: add build scripts

---
+++ scripts/linux.sh
@@ -0,0 +1,6 @@
+# Command to compile the code for Linux ✨
+# ⚠ You'll need to install `clang` and have it in $PATH ⚠
+
+# Run the generated executable using `./main` 📝
+
+clang++ ../src/main.cc -o ../main
---
+++ scripts/windows.bat
@@ -0,0 +1,6 @@
+rem Command to compile the code for Windows ✨
+rem ⚠ You'll need to install `clang` and have it in PATH ⚠
+
+rem Run the generated executable using `main` 📝
+
+clang++ ../src/main.cc -o ../main.exe
